c60905713259798ffd50d3dd45b80beefa2b549f
Endrik Santana Garcia da Costa <116817681+Pentriks@users.noreply.github.com>
2023-07-20 17:51:21 -0300

    Interpolação de dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.scss']
 })
 export class FirstComponentComponent {
-
+ name: string = 'Matheus';
+ age: number = 30;
+ job = 'Programador';
+ hobbies = ['Correr', 'Jogar', 'Estudar'];
+ car = {
+  name: 'Polo',
+  year: 2019,
+ };
 }
