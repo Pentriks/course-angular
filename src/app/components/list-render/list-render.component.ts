cdda705f822cd01fbfec2b31a6a1e6430568bae0
Endrik Santana Garcia da Costa <116817681+Pentriks@users.noreply.github.com>
2023-07-24 15:00:23 -0300

    List rendering

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
 
 @Component({
   selector: 'app-list-render',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.scss']
 })
 export class ListRenderComponent {
-
+  weapons = [
+    { name: 'Kalashnikov', type: 'Machine gun' },
+    { name: 'Uzi', type: 'Machine gun' },
+    { name: 'M4', type: 'Machine gun' },
+    { name: 'Ceska', type: 'Machine gun' },
+  ];
 }
