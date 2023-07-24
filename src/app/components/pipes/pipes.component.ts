d66c16015705c740b4d3204acebd5c620954ca5a
Endrik Santana Garcia da Costa <116817681+Pentriks@users.noreply.github.com>
2023-07-24 16:41:27 -0300

    pipes

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.scss']
+})
+export class PipesComponent {
+  someText = 'Testing the operator pipe!';
+
+  today = new Date();
+
+}
